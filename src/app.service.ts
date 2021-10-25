85cd0539f6cbee94fe82c6ee78b7b78594144296
lysar1e <79013918+lysar1e@users.noreply.github.com>
2021-10-26 03:10:03 +0600

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! New Version 2.2;
+    return 'Hello World! New Version 2.2';
   }
 }
