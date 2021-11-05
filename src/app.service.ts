03176e7454bae9e89123a44b4226b3b8565aad26
Yerassyl Kaliyev <yerassylkaliyev@iMac-Yerassyl.local>
2021-11-06 05:01:37 +0600

    Minor changes.

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'What are you doing here? Bye';
+  getHello(): string[] {
+    return ['What are you doing here? Bye'];
   }
 }
